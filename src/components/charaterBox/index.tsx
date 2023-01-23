f60c0d3c70a9393aeb62cb58482f95269c08c5ab
Aboa123 <wlxm9999@gmail.com>
2023-01-23 17:27:15 +0900

    fix: 캐릭터박스 컴포넌트 UI상세조정

--- src/components/charaterBox/index.tsx
+++ src/components/charaterBox/index.tsx
@@ -16,12 +16,19 @@ const CharaterBox = (props: CharaterBoxProps) => {
         boxSizing: "border-box",
         position: "relative",
         cursor: "pointer",
+        overflow: "hidden",
+        backgroundColor: "#30353f",
       }}
       onClick={props.onPress}
     >
       <Image
         src={props.image as any}
-        style={{ width: 150, height: 250, objectFit: "cover" }}
+        style={{
+          width: 150,
+          height: 250,
+          objectFit: "cover",
+          transform: "scale(1.2)",
+        }}
         preview={false}
       />
       <span
@@ -35,7 +42,7 @@ const CharaterBox = (props: CharaterBoxProps) => {
           position: "absolute",
           bottom: 0,
           height: 35,
-          fontSize: 25,
+          fontSize: 20,
         }}
       >
         {props.name}
